d71d67e69381f0a49b14bce6ba01a4fb3adeb06c
Martin Planer <martin.planer@gmail.com>
2017-09-21 13:34:37 +0200

    Implemented RSS item Scanner

---
+++ internal/felix/rss/scanner.go
@@ -0,0 +1,31 @@
+package rss
+
+import (
+	"context"
+	"io"
+
+	"github.com/pkg/errors"
+
+	"github.com/martinplaner/felix/internal/felix"
+	"github.com/mmcdole/gofeed"
+)
+
+// ItemScanner parses r as an RSS feed and extracts all feed items.
+var ItemScanner = felix.ScanFunc(func(ctx context.Context, r io.Reader, e felix.Emitter) error {
+	fp := gofeed.NewParser()
+	feed, err := fp.Parse(r)
+
+	if err != nil {
+		return errors.Wrap(err, "could not parse RSS feed")
+	}
+
+	for _, item := range feed.Items {
+		e.EmitItem(felix.Item{
+			Title:   item.Title,
+			URL:     item.Link,
+			PubDate: *item.PublishedParsed,
+		})
+	}
+
+	return nil
+})
---
+++ internal/felix/rss/scanner_test.go
@@ -0,0 +1,74 @@
+package rss
+
+import (
+	"context"
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"github.com/martinplaner/felix/internal/felix/mock"
+)
+
+var (
+	empty      = ""
+	invalidRSS = "<html></html>"
+)
+
+func TestRSSScanner(t *testing.T) {
+	testCases := []struct {
+		desc          string
+		content       string
+		expectedItems int
+		wantError     bool
+	}{
+		{
+			desc:          "invalid feed",
+			content:       invalidRSS,
+			expectedItems: 0,
+			wantError:     true,
+		},
+		{
+			desc:          "empty feed",
+			content:       empty,
+			expectedItems: 0,
+			wantError:     true,
+		},
+		{
+			desc:          "valid feed",
+			content:       mustReadFile(t, "testdata/feed_rss_utf8.xml"),
+			expectedItems: 3,
+			wantError:     false,
+		},
+	}
+
+	ctx := context.Background()
+
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			e := &mock.Emitter{}
+			r := strings.NewReader(tC.content)
+			err := ItemScanner.Scan(ctx, r, e)
+
+			if (err != nil) != tC.wantError {
+				t.Errorf("unexpected error return value! wantError = %v, got: %v", tC.wantError, err)
+			}
+
+			if len(e.Items) != tC.expectedItems {
+				t.Errorf("invalid number of items found! expected: %v, got: %v", tC.expectedItems, len(e.Items))
+			}
+		})
+	}
+}
+
+// read file content to string, fatal on error
+func mustReadFile(t *testing.T, filename string) string {
+	t.Helper()
+
+	b, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		t.Fatal("could not read file:", err)
+	}
+
+	return string(b)
+}
