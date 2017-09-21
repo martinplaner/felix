61cb3637737849cfe3419985f3b997d17d020260
Martin Planer <martin.planer@gmail.com>
2017-09-21 13:06:12 +0200

    Implemented HTML link scanner

---
+++ internal/felix/html/scanner.go
@@ -0,0 +1,38 @@
+package html
+
+import (
+	"context"
+	"io"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/pkg/errors"
+
+	"github.com/martinplaner/felix/internal/felix"
+)
+
+// LinkScanner parses r as an HTML document and extracts all links.
+var LinkScanner = felix.ScanFunc(func(ctx context.Context, r io.Reader, e felix.Emitter) error {
+	doc, err := goquery.NewDocumentFromReader(r)
+
+	if err != nil {
+		return errors.Wrap(err, "could not read HTML document")
+	}
+
+	doc.Find("a").Each(func(index int, item *goquery.Selection) {
+		if href, ok := item.Attr("href"); ok {
+			title := item.Text()
+			if strings.TrimSpace(title) == "" {
+				title = href
+			}
+			e.EmitLink(felix.Link{
+				Title: title,
+				URL:   href,
+			})
+		}
+	})
+
+	// TODO: Also parse by regex to capture non <a> links? (but maybe in separate package/scanner?)
+
+	return nil
+})
---
+++ internal/felix/html/scanner_test.go
@@ -0,0 +1,86 @@
+package html
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/martinplaner/felix/internal/felix/mock"
+)
+
+var empty = ""
+
+// HTML parser wont throw error even with this garbage!?
+// var invalidHTML = `!@#^%$#)~+)_(*$%?><":LPO:öœø}}{:}{:>>?</.})`
+
+var noLinks = `
+<html>
+<body>
+</body>
+</html>
+`
+
+var twoLinkTags = `
+<html>
+	<body>
+		<a href="http://example.com">Testlink1</a>
+		<div>
+			<a href="http://example.org">Testlink2</a>
+		</div>
+
+		<p>Hey look what I found on http://example.net.<p>
+	</body>
+</html>
+`
+
+func TestLinkScanner(t *testing.T) {
+	testCases := []struct {
+		desc          string
+		content       string
+		expectedLinks int
+		shouldError   bool
+	}{
+		{
+			desc:          "empty document",
+			content:       empty,
+			expectedLinks: 0,
+			shouldError:   false,
+		},
+		// {
+		// 	desc:          "invalid HTML",
+		// 	content:       invalidHTML,
+		// 	expectedLinks: 0,
+		// 	shouldError:   true,
+		// },
+		{
+			desc:          "no links",
+			content:       noLinks,
+			expectedLinks: 0,
+			shouldError:   false,
+		},
+		{
+			desc:          "two link tags",
+			content:       twoLinkTags,
+			expectedLinks: 2,
+			shouldError:   false,
+		},
+	}
+
+	ctx := context.Background()
+
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			e := &mock.Emitter{}
+			r := strings.NewReader(tC.content)
+			err := LinkScanner.Scan(ctx, r, e)
+
+			if (err != nil) != tC.shouldError {
+				t.Errorf("unexpected error return value! shouldError = %v, got: %v", tC.shouldError, err)
+			}
+
+			if len(e.Links) != tC.expectedLinks {
+				t.Errorf("invalid number of links found! expected: %v, got: %v", tC.expectedLinks, len(e.Links))
+			}
+		})
+	}
+}
