014550c672cee656e51492f33a8104a4c583c4ad
Martin Planer <martin.planer@gmail.com>
2017-09-30 19:21:55 +0200

    Added NextFetchFunc to Fetcher

--- internal/felix/fetcher.go
+++ internal/felix/fetcher.go
@@ -15,13 +15,16 @@ type Fetcher struct {
 	url       string
 	source    Source
 	scanner   Scanner
-	nextFetch func() (time.Duration, bool)
+	nextFetch NextFetchFunc
 	items     chan<- Item
 	links     chan<- Link
 	log       Logger
 }
 
-func NewFetcher(url string, source Source, scanner Scanner, nextFetch func() (time.Duration, bool), items chan<- Item, links chan<- Link) *Fetcher {
+// NextFetchFunc returns if the fetcher should continue to fetch and if so, how long to wait before the next attempt.
+type NextFetchFunc func(url string) (bool, time.Duration)
+
+func NewFetcher(url string, source Source, scanner Scanner, nextFetch NextFetchFunc, items chan<- Item, links chan<- Link) *Fetcher {
 	return &Fetcher{
 		url:       url,
 		source:    source,
@@ -41,6 +44,8 @@ func (f *Fetcher) SetLogger(log Logger) {
 // Start starts the fetching.
 func (f *Fetcher) Start(quit <-chan struct{}) {
 
+	f.log.Info("started fetcher", "url", f.url)
+
 	bg := context.Background()
 	e := &emitter{
 		items: f.items,
@@ -49,16 +54,17 @@ func (f *Fetcher) Start(quit <-chan struct{}) {
 
 L:
 	for {
-		nextFetch, shouldContinue := f.nextFetch()
+		shouldContinue, nextFetch := f.nextFetch(f.url)
 		if !shouldContinue {
 			f.log.Info("will not try to continue. quitting.", "url", f.url)
 			return
 		}
+		f.log.Info("waiting", "nextFetch", nextFetch, "url", f.url)
 
 		select {
 		// TODO: abstract time stdlib away into clock, etc. for testing
 		case <-time.After(nextFetch):
-			ctx, cancel := context.WithTimeout(bg, 20*time.Second)
+			ctx, cancel := context.WithTimeout(bg, 15*time.Second)
 			e.EmitFollow(f.url)
 
 			for e.HasFollow() {
@@ -93,5 +99,51 @@ L:
 		case <-quit:
 			break L
 		}
+	}
+}
+
+// FibWait returns a NextFetchFunc that
+func FibWait(ds Datastore, baseInterval time.Duration, maxTries int) NextFetchFunc {
+	var fib func(n int) int
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+		return fib(n-2) + fib(n-1)
+	}
+
+	return func(url string) (bool, time.Duration) {
+		lastTry, previousTries, err := ds.AddTry(url)
+
+		if err != nil {
+			// TODO: re-evaluate this decision. this silently hides an error :-/
+			return true, baseInterval
+		}
+
+		if previousTries >= maxTries {
+			return false, time.Duration(0)
+		}
+
+		interval := time.Duration(fib(previousTries)) * baseInterval
+		nextTry := lastTry.Add(interval)
+		untilNext := nextTry.Sub(time.Now())
+
+		return true, untilNext
+	}
+}
+
+func PeriodicWait(ds Datastore, fetchInterval time.Duration) NextFetchFunc {
+	return func(url string) (bool, time.Duration) {
+		lastTry, _, err := ds.AddTry(url)
+
+		if err != nil {
+			// TODO: re-evaluate this decision. this silently hides an error :-/
+			return true, fetchInterval
+		}
+
+		nextTry := lastTry.Add(fetchInterval)
+		untilNext := nextTry.Sub(time.Now())
+
+		return true, untilNext
 	}
 }
