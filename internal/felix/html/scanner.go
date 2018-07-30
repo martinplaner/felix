7ebb1e92e3f8d357844f9c94e4cb4d7819b4e9ce
Martin Planer <martin.planer@gmail.com>
2018-07-30 17:50:11 +0200

    Implemented additional HTML link scanning via regex for text-based links.

--- internal/felix/html/scanner.go
+++ internal/felix/html/scanner.go
@@ -7,6 +7,7 @@ package html
 import (
 	"context"
 	"io"
+	"regexp"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -15,7 +16,11 @@ import (
 	"github.com/martinplaner/felix/internal/felix"
 )
 
+var urlPattern = regexp.MustCompile(`(?mi:https?://[^<"' (\r\n)\t]+)`)
+
 // LinkScanner parses r as an HTML document and extracts all links.
+// Links are uniquely identified by the links URL. Multiple instances of the same URL (e.g. href),
+// will only be reported once (i.e. the first found instance).
 var LinkScanner = felix.ScanFunc(func(ctx context.Context, r io.Reader, e felix.Emitter) error {
 	doc, err := goquery.NewDocumentFromReader(r)
 
@@ -23,12 +28,17 @@ var LinkScanner = felix.ScanFunc(func(ctx context.Context, r io.Reader, e felix.
 		return errors.Wrap(err, "could not read HTML document")
 	}
 
+	// TODO: only retrieve absolute links with schema?
+	foundURLs := make(map[string]bool)
+
+	// Look for proper HTML links in <a> elements
 	doc.Find("a").Each(func(index int, item *goquery.Selection) {
-		if href, ok := item.Attr("href"); ok {
+		if href, ok := item.Attr("href"); ok && !foundURLs[href] {
 			title := item.Text()
 			if strings.TrimSpace(title) == "" {
 				title = href
 			}
+			foundURLs[href] = true
 			e.EmitLink(felix.Link{
 				Title: title,
 				URL:   href,
@@ -36,7 +46,19 @@ var LinkScanner = felix.ScanFunc(func(ctx context.Context, r io.Reader, e felix.
 		}
 	})
 
-	// TODO: Also parse by regex to capture non <a> links? (but maybe in separate package/scanner?)
+	// Look for other links in complete source via regex
+	if s, err := doc.Html(); err == nil {
+		urls := urlPattern.FindAllString(s, -1)
+		for _, u := range urls {
+			if !foundURLs[u] {
+				foundURLs[u] = true
+				e.EmitLink(felix.Link{
+					Title: u,
+					URL:   u,
+				})
+			}
+		}
+	}
 
 	return nil
 })
